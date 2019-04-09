fbdf3f43eb9e14eab2c8c022b31d161d55aaeb29
Leo Rønsholt Mikkelsen <leomikkelsen9@gmail.com>
2019-04-09 12:29:58 +0200

    Merge

--- KemiTest/ParentNodes/TekstFelt.cs
+++ KemiTest/ParentNodes/TekstFelt.cs
@@ -1,44 +1,38 @@
 using Godot;
 using System;
-using System.Windows.Input;
-using System.Windows;
+
 
 public class TekstFelt : LineEdit
 {
     // Declare member variables here. Examples:
     // private int a = 2;
     // private string b = "text";
-    public static void Print(string str)
-    {
-      Console.Write(str);
-    }
+    
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         /*Secret = true;
         SecretCharacter = " ";*/
-        Print(GetName());
+        GD.Print(GetName());
+        GD.Print(InputMap.GetActions());
     }
 
     float time = 0;
     public override void _Process(float delta)
     {
-      if (Keyboard.IsKeyToggled(Key.Escape))
-      {
-          this.Dispose();
-
-      }
-      time += delta;
-      if (time>=4)
-      {
-        time -= 4;
-        Text="UwU";
-      }
-      else
-      {
-        Text="OwO";
-      }
       
+
+     /* if (Input.IsActionPressed("ui_change_text_one"))
+      {
+        this.Text += "hej";
+        hej = true;
+      }
+
+      if (Input.IsActionPressed("ui_change_text_two"))
+      {
+        this.Text += "OwO";
+        hej = false;
+      }*/
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
--- KemiTest/Scripts/BaseScript.cs
+++ KemiTest/Scripts/BaseScript.cs
@@ -6,16 +6,27 @@ public class BaseScript : Node2D
     // Declare member variables here. Examples:
     // private int a = 2;
     // private string b = "text";
-
+    Node red;
+    Node green;
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+      red = GetNode("Red");
+      green = GetNode("Green");
+      GD.Print(GetNode("Red"));
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+ // Called every frame. 'delta' is the elapsed time since the previous frame.
+  public override void _Process(float delta)
+  {
+      
+  }
+  public override void _Draw()
+  {
+    
+    DrawLine(((Node2D)GetNode("Red")).Position, ((Node2D)GetNode("Green")).Position,new Color(0,0,0), 10, true);
+
+  }
+
+
 }
